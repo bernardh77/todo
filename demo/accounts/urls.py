54aa24f3fe82e9678238cff30a7f3857a546ac31
bernard haryanto <haryantobernard77@gmail.com>
2025-02-26 17:17:40 +1100

    Forgot password done

--- demo/accounts/urls.py
+++ demo/accounts/urls.py
@@ -1,9 +1,18 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('toggle-theme/', views.toggle_theme, name='toggle_theme'),
+    path('password-reset/', views.password_reset_request, name='password_reset'),
+    path('password-reset/done/', views.password_reset_done, name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', 
+         views.CustomPasswordResetConfirmView.as_view(), 
+         name='password_reset_confirm'),
+    path('reset/done/', 
+         auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'),
+         name='password_reset_complete'),
 ]
